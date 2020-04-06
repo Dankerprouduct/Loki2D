3556c452dec16383ed25cc65d055aa630590da7a
David William Fitzgerald <dankerprouduct2@gmail.com>
2020-04-05 21:02:29 -0400

    added a json helper

---
+++ Loki2D/Core/Utilities/JsonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Loki2D.Core.Utilities
+{
+    public static class JsonHelper
+    {
+        
+        public static void Save(object data, string path)
+        {
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.Write(json);
+            }
+        }
+
+        public static T Load<T>(string path)
+        {
+            var obj = JsonConvert.DeserializeObject<T>(path);
+            return obj;
+        }
+    }
+}
