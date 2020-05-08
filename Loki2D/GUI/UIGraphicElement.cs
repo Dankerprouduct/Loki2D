ea0c9795ebf8622ce0843c8003bba996734e9ee3
David William Fitzgerald <dankerprouduct2@gmail.com>
2020-05-08 14:16:12 -0400

    Create UIGraphicElement.cs

---
+++ Loki2D/GUI/UIGraphicElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loki2D.GUI
+{
+    public class UIGraphicElement
+    {
+        /// <summary>
+        /// The path for the graphic
+        /// </summary>
+        public string GraphicPath { get; set; }
+
+        /// <summary>
+        /// Sets the path for the graphic
+        /// </summary>
+        /// <param name="path"></param>
+        public void SetPath(string path)
+        {
+            GraphicPath = path;
+        }
+    }
+}
