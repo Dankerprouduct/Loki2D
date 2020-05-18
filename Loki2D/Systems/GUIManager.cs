f858372668fd57b1f61f616c85a88019552bb5f9
David William Fitzgerald <dankerprouduct2@gmail.com>
2020-05-18 18:27:36 -0400

    Create GUIManager.cs

---
+++ Loki2D/Systems/GUIManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loki2D.GUI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Loki2D.Systems
+{
+    public class GUIManager: SystemManager<GUIManager>
+    {
+        /// <summary>
+        /// Holds elements displayed on screen
+        /// </summary>
+        private List<UIElement> _uiElements;
+
+        public GUIManager()
+        {
+
+        }
+
+        /// <summary>
+        /// Adds UI Element to be rendered and updated
+        /// </summary>
+        /// <param name="uiElement"></param>
+        public void AddElement(UIElement uiElement)
+        {
+            _uiElements.Add(uiElement);
+        }
+
+        /// <summary>
+        /// Removes UI Element
+        /// </summary>
+        /// <param name="uiElement"></param>
+        public void RemoveElement(UIElement uiElement)
+        {
+            if (_uiElements.Contains(uiElement))
+            {
+                RemoveElement(uiElement);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var element in _uiElements)
+            {
+                if (element.CanUpdate)
+                {
+
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var element in _uiElements)
+            {
+                if (element.CanDraw)
+                {
+
+                }
+            }
+            base.Draw(spriteBatch);
+        }
+
+
+
+    }
+}
