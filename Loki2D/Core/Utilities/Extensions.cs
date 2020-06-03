e0a3a8603570808a199d900a5c5c3baf293236cf
David William Fitzgerald <dankerprouduct2@gmail.com>
2020-06-03 17:29:26 -0400

    Update Extensions.cs

--- Loki2D/Core/Utilities/Extensions.cs
+++ Loki2D/Core/Utilities/Extensions.cs
@@ -19,5 +19,24 @@ namespace Loki2D.Core.Utilities
             var index = MathUtils.Random.Next(0, list.Count);
             return list[index];
         }
+
+        /// <summary>
+        /// Fills a list with random integer values
+        /// </summary>
+        /// <param name="list">the list to be modified</param>
+        /// <param name="values">the amount of of values to be added</param>
+        /// <param name="minValue">The min value to be added</param>
+        /// <param name="maxValue">The max value to be added</param>
+        /// <returns></returns>
+        public static List<int> FillRandomInt(this List<int> list, int values, int minValue = 0, int maxValue = 100)
+        {
+            for (int i = 0; i < values; i++)
+            {
+                var newValue = MathUtils.Random.Next(minValue, maxValue);
+                list.Add(newValue);
+            }
+
+            return list;
+        }
     }
 }
