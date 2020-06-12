623294eacb034d9d41342b16cd07c63d1e2ff962
David William Fitzgerald <dankerprouduct2@gmail.com>
2020-06-11 21:45:35 -0400

    moved timer

--- Loki2D/Core/Utilities/Timer.cs
+++ Loki2D/Core/Utilities/Timer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Loki2D.Systems
+﻿namespace Loki2D.Core.Utilities
 {
     public class Timer
     {
