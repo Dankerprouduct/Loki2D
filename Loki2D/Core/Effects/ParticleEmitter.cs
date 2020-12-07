b92478e6d2f9fea168c0d29222c42621b004e9dc
David Fitzgerald <dankerprouduct2@gmail.com>
2020-12-07 00:16:58 -0500

    Update ParticleEmitter.cs

--- Loki2D/Core/Effects/ParticleEmitter.cs
+++ Loki2D/Core/Effects/ParticleEmitter.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX.X3DAudio;
 
 namespace Loki2D.Core.Effects
 {
     public class ParticleEmitter
     {
+
+        /// <summary>
+        /// The speed of this emitter in miliseconds
+        /// </summary>
+        public float EmissionSpeed { get; set; }
+        
+
+
+        public ParticleEmitter()
+        {
+            
+        }
     }
 }
